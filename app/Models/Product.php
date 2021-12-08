fabdb31578b48003e62480091ce0da20d6a61c2b
NNChau <nguyenngocchau2804@gmail.com>
2021-12-08 14:31:34 +0700

    add error

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,10 +9,12 @@ class Product extends Model
 {
     use HasFactory;
     protected $table="products";
-    public function user(){
-        return $this->belongsTo('App\Models\User','user_id','id');
+
+    public function user() {
+        return $this->belongsTo('App\Models\User', 'user_id', 'id');
     }
-    public function category(){
-        return $this->belongsTo('App\Models\Category','cat_id','id');
+
+    public function category() {
+        return $this->belongsTo('App\Models\Category', 'cat_id', 'id');
     }
 }
